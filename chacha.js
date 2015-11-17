745e510b0f3ae813830f39c448597a18336777cc
Martyn Eggleton <meggleton@utcsheffield.org.uk>
2015-11-17 16:42:30 +0000

    first code

---
+++ chacha.js
@@ -0,0 +1,17 @@
+var autonomy = require('ardrone-autonomy');
+var mission  = autonomy.createMission();
+
+mission.takeoff()
+       // Write it here
+       .land();
+
+mission.run(function (err, result) {
+    if (err) {
+        console.trace("Oops, something bad happened: %s", err.message);
+        mission.client().stop();
+        mission.client().land();
+    } else {
+        console.log("Mission success!");
+        process.exit(0);
+    }
+});
