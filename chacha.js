d562e3fee2b4441f7241b2f6465232b23d5ae44a
Martyn Eggleton <martyn.eggleton@gmail.com>
2015-11-17 20:33:54 +0000

    Adds the dance moves from Callam M, Declan and Laurens group

--- chacha.js
+++ chacha.js
@@ -2,9 +2,19 @@ var autonomy = require('ardrone-autonomy');
 var mission  = autonomy.createMission();
 
 mission.takeoff()
-       // Write it here
-       .land();
-
+       .zero()
+       .altitude(1)
+       .left(1)
+       .right(1)
+       .backward(1)
+       .up(1)
+       .down(1)
+       .go({x:-0.5,y:0,z:-0.5})
+       .go({x:0.5,y:0,z:0.5})
+       .go({x:0.5,y:0,z:0.5})
+       .go({x:-0.5,y:0,z:-0.5})
+       .land(); 
+      
 mission.run(function (err, result) {
     if (err) {
         console.trace("Oops, something bad happened: %s", err.message);
