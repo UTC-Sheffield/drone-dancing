45869ecb4e0e9fa568b9eb9ddad61ab5a73ca23e
Mr Eggleton <meggleton@utcsheffield.org.uk>
2016-06-01 20:05:24 +0100

    Tidies up dances

--- dance.js
+++ dance.js
@@ -4,6 +4,8 @@
 var ardrone = require('ar-drone');
 var autonomy = require('ardrone-autonomy');
 var client  = ardrone.createClient({});
+var temporal = require('temporal');
+
 
 /* Adds a dummy distance and a callback to fit all the other moves
  * Sets the goal to the current state and attempt to hover on top.
@@ -17,6 +19,7 @@ autonomy.Controller.prototype.return = function(dist, callback) {
     this._go({x: 0, y: 0, z: 1, yaw: 0}, callback);
 }
 
+//Our version of Controller which executes the callback of a move as it is replaced
 autonomy.Controller.prototype._go = function(goal, callback) {
     // Since we are going to modify goal settings, we
     // disable the controller, just in case.
@@ -57,10 +60,6 @@ autonomy.Controller.prototype._go = function(goal, callback) {
 
 var oController = new autonomy.Controller(client, {});
 
-var temporal = require('temporal');
-var landingTimer = null;
-
-
 temporal.on("idle", function() {
 	console.log("Temporal is idle : Landing");
 	client.land(function(){
@@ -83,19 +82,18 @@ process.on("exit", function() {
 oController.EPS_LIN      = 0.2; // We are ok with 10 cm horizontal precision
 oController.EPS_ALT      = 0.2; // We are ok with 10 cm altitude precision
 oController.EPS_ANG      = 0.2; // We are ok with 0.1 rad precision (5 deg)
-
 oController.STABLE_DELAY = 30; // Time in ms to wait before declaring the drone on target
 
 
 var fs = require('fs'); //Get the node library that can do File System stuff
 
-var FileData = fs.readFileSync("chacha2.json").toString(); //Read the File put it in a string
-//var FileData = fs.readFileSync("test_dance.json").toString(); //Read the File put it in a string
-
-var DanceData = JSON.parse(FileData); //Take the string and read the file into a big javascript array
+var FileData = fs.readFileSync("dances/ccs.json").toString(); //Read the File put it in a string
+//var FileData = fs.readFileSync("dances/test.json").toString(); //Read the File put it in a string
 
 var BeatsFileData = fs.readFileSync("beats/ccs.json").toString(); //Read the File put it in a string
 
+
+var DanceData = JSON.parse(FileData); //Take the string and read the file into a big javascript array
 var BeatsData = JSON.parse(BeatsFileData); //Take the string and read the file into a big javascript array
 var Features = JSON.parse(BeatsData.features);
 console.log("DanceData", DanceData.length);
@@ -109,13 +107,10 @@ console.log("DanceData", DanceData.length);
 	return step;
 });*/
 
-
 //When a logs when a step ends so we can spot the steps that don't complete
 function onStepEnd(stepid, move, params, beats){
   console.log("Step",stepid, "Complete", this._goal.reached, move, params, beats);
 }
-
-var Delay = 2000; 
 
 var Beats = Features.timeline.beat.map(function(Beat){
 	return Beat;
@@ -124,16 +119,23 @@ var Beats = Features.timeline.beat.map(function(Beat){
 var currentBeat = 0;
 var LastBeatTime = 0;
 var Moves = DanceData.map(function(step, stepid){
+	//What is the next beat
 	var thisBeat = Features.timeline.beat[currentBeat];
+	//What tine is it and how long since the last beat we used in milliseconds
 	var time = Math.round(thisBeat.time*1000);
 	var BeatDelay = time - LastBeatTime;
 	LastBeatTime = time;
 	
 	var thisBeat = Beats[currentBeat];
 	
+	//The next step is a oController function with the same name as the move set to run with a  function that calls onStepEnd with the info of this step
 	var NextStep = oController[step.move].bind(oController, step.param, onStepEnd.bind(oController, stepid, step.move, step.param, step.beats));
-	console.log(BeatDelay, step);
+	//console.log(BeatDelay, step);
+	
+	//How many beats should this move take
 	currentBeat += step.beats; 
+	
+	//Each step starts at a fixed time from the start of the last move 
 	return {
         delay: BeatDelay,
         task: NextStep
