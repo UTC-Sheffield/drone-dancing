bd065911b19a43904b5b565539229dc321921326
Martyn Eggleton <martyn.eggleton@gmail.com>
2016-06-19 22:04:48 +0100

    Progress on editor

--- edit.js
+++ edit.js
@@ -18,25 +18,44 @@ function Start(){
 	var MovesFileData = fs.readFileSync("dances/"+sSong+".json").toString(); //Read the File put it in a string
 	var MovesData = JSON.parse(MovesFileData); //Take the string and read the file into a big javascript array
 	
+	var iBeat = iStep =0;
+	var Steps = MovesData.map(function(move) { 
+		move.step = iStep;
+		move.beat = iBeat;
+		iBeat += move.beats;
+		iStep ++;
+		return move; 
+	});
+	
 	var FileData = fs.readFileSync(""+sSong+"_lyrics.json").toString(); //Read the File put it in a string
-	var Data = JSON.parse(FileData); //Take the string and read the file into a big javascript array
+	var LyricData = JSON.parse(FileData); //Take the string and read the file into a big javascript array
 	
-	loadBeats(sSong)
-	
-	viewModel = new LyricModel(MovesData, Data);
-	ko.applyBindings(viewModel);
-	
-	ePlayer.play();
-}
-
-function loadBeats(sSong){
 	var BeatsFileData = fs.readFileSync("beats/"+sSong+".json").toString(); //Read the File put it in a string
 	var BeatsData = JSON.parse(BeatsFileData); //Take the string and read the file into a big javascript array
 	var Features = JSON.parse(BeatsData.features);
-	
-	Beats = Features.timeline.beat.map(function(Beat){
+	LyricData.reverse();
+	 
+	Beats = Features.timeline.beat.map(function(Beat, iBeat){
+		var PossibleLyrics = LyricData.filter(function(oLyric){
+			return oLyric.beat <= iBeat;
+		});
+		
+		//console.log("Beat",Beat, "iBeat", iBeat, "PossibleLyrics[0]", PossibleLyrics[0]);
+		Beat.beat = iBeat;
+		Beat.lyric = PossibleLyrics[0].lyric;
+		Beat.lyricbeat = PossibleLyrics[0].beat;
 		return Beat;
 	});
+	
+	//console.log(Beats);
+	
+	viewModel = new StepsModel(Steps, Beats);
+	ko.applyBindings(viewModel);
+	
+	//ePlayer.play();
+}
+
+function loadBeats(sSong){
 	return Beats;
 }
 
@@ -51,32 +70,37 @@ function Stop(){
 
 var iBeatTime = 0;
 
-var LyricModel = function(Moves, lines) {
+var StepsModel = function(Steps, LyricBeats) {
     var self = this;
-    self.moves = ko.observableArray(Moves);
-    self.lines = ko.observableArray(lines);
- 
-	self.availableMoves = ko.observableArray(["up", "down", "forward", "backward",""]);
- 
-    self.addLyric = function() {
-		var iTime = Math.round(ePlayer.currentTime * 1000);
+    self.moves = ko.observableArray(Steps);
+    
+    self.moveslyrics = self.moves.map(function(move) { 
+		console.log(move);
+		move.time = LyricBeats[move.beat].time;
+		move.lyric = LyricBeats[move.beat].lyric;
+		move.lyricbeat = LyricBeats[move.beat].lyricbeat;
 		
-		do {
-			iCurrentBeat ++;
-			iBeatTime = Math.round(Beats[iCurrentBeat].time * 1000);
-		} while (iBeatTime < iTime)
-		
-		iCurrentBeat --;
-		iBeatTime = Math.round(Beats[iCurrentBeat].time * 1000);
-        
-        self.lines.push({
-            lyric: aLyrics[iLine],
-            beat: iCurrentBeat
-        });			
-		iLine++;
-		showNextLine();
-    };
+		return move; 
+	});
+	
+	
+    self.availableMoves = ko.observableArray([
+		"up", 
+		"down", 
+		"forward", 
+		"backward",
+		"left",
+		"right",
+		"hover",
+		"cw",
+		"ccw",
+		"altitude",
+		"go",
+		"move"]);
  
+    self.addMove = function() {
+	};
+	
     self.playFrom = function(Lyric) {
 		var iNewTime = Beats[Lyric.beat].time;
 		//console.log(iNewTime);
@@ -85,6 +109,7 @@ var LyricModel = function(Moves, lines) {
         setTimeout(function(){ePlayer.pause();}, 4000);
     };
  
+	
     self.save = function(form) {
 		var sSong = document.getElementById("song").value; //ccs /test
 	
